c4ebd8e823835bc0ea386743fcaa0cde213fdc05
HI-SAYA <qnfnsk45@naver.com>
2023-09-18 20:09:02 +0900

    0918

--- src/main/java/com/ohgiraffers/async/JsonDataController.java
+++ src/main/java/com/ohgiraffers/async/JsonDataController.java
@@ -6,11 +6,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Controller
 public class JsonDataController {
 
+    private final List<UserDTO> userList;
+
+    public JsonDataController(){
+        userList = new ArrayList<>();
+        userList.add(new UserDTO("1", "유관순", 16, "yoo123@ohgiraffers.com", new Date()));
+        userList.add(new UserDTO("2", "홍길동", 20, "hong123@ohgiraffers.com", new Date()));
+        userList.add(new UserDTO("3", "신사임당", 30, "shin123@ohgiraffers.com", new Date()));
+    }
     @GetMapping("/json-data")
     public String showJsonData() {
 
@@ -27,10 +37,29 @@ public class JsonDataController {
     * UserDTO{id='null', nickname='하이', age=30, email='qnfnsk45@naver.com', registDate=Tue Sep 29 09:00:00 KST 2020}
     * */
     @PostMapping("/jquery/json")
-    public @ResponseBody String jqueryJsonTest(@RequestBody UserDTO requestBody) {
-
+    public @ResponseBody String jqueryJsonTest(@RequestBody List<UserDTO> requestBody) {
+// 여러명(여기서는 3명) 전달할때 @RequestBody 뒤에 UserDTO 타입을 List<UserDTO>로 바꾼다.
         System.out.println("requestBody : " + requestBody);
 
         return "success";
+    }
+
+    @PostMapping("/fetch/json")
+    public @ResponseBody String fetchJsonTest(@RequestBody List<UserDTO> requestBody) {
+        System.out.println("requestBody : " + requestBody);
+
+        return "success";
+    }
+
+    @GetMapping("/jquery/json")
+    public @ResponseBody List<UserDTO> jqueryJsonTest(){
+
+        return userList;
+    }
+
+    @GetMapping("/fetch/json")
+    public @ResponseBody List<UserDTO> fetchJsonTest(){
+
+        return userList;
     }
 }
